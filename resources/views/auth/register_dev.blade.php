c8b8c28f12699a9543ea9ad4e0e1d7ff1821c3a1
cherki-hamza <cherki0hamza@gmail.com>
2024-08-12 12:09:44 +0400

    new last update 08-12-2024

--- resources/views/auth/register_dev.blade.php
+++ resources/views/auth/register_dev.blade.php
@@ -4,15 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Register - {{ ENV('APP_NAME') }}</title>
-        <link rel="icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png" />
+        <link rel="icon" href="{{ asset('public/assets/images/favicon.png') }}" type="image/png" />
         <!-- Google Font: Source Sans Pro -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="{{ asset('template/admin/plugins/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('public/template/admin/plugins/fontawesome-free/css/all.min.css') }}">
         <!-- icheck bootstrap -->
-        <link rel="stylesheet" href="{{ asset('template/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('public/template/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
         <!-- Theme style -->
-        <link rel="stylesheet" href="{{ asset('template/admin/dist/css/adminlte.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('public/template/admin/dist/css/adminlte.min.css') }}">
         {!! htmlScriptTagJsApi() !!}
     </head>
     <body class="hold-transition login-page">
@@ -86,10 +86,10 @@
         @include('sweetalert::alert')
         <!-- /.login-box -->
         <!-- jQuery -->
-        <script src="{{ asset('template/admin/plugins/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('public/template/admin/plugins/jquery/jquery.min.js') }}"></script>
         <!-- Bootstrap 4 -->
-            <script src="{{ asset('template/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+            <script src="{{ asset('public/template/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
         <!-- AdminLTE App -->
-        <script src="{{ asset('template/admin/dist/js/adminlte.js') }}"></script>
+        <script src="{{ asset('public/template/admin/dist/js/adminlte.js') }}"></script>
     </body>
 </html>
